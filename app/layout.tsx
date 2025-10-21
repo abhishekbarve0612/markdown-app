8ee5b115158b2ee2c17f42bdaae1499aaafbdcd4
Abhishek Barve <barveabhishek.ab@gmail.com>
2025-10-21 14:39:54 +0530

    Add Favicon and Web Manifest: Introduced new favicon images, including PNG and SVG formats, and added a web app manifest for improved branding and mobile support. Updated layout to link these assets for better user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ThemeScript />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <DarkModeProvider>{children}</DarkModeProvider>
